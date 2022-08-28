93a40da20542cd878fc9c791fb1db06ff9e98b9c
Michael-Mehta <supermichael2001@gmail.com>
2022-08-27 21:23:27 -0700

    game.js

--- game.js
+++ game.js
@@ -6,6 +6,82 @@
 //I will then use if else statements to return a either
 //rock, paper or scissors depending on which value is produced
 
+let wins = 0;
+    let computerScore = 0;
+
+
+const btnOne = document.querySelector("#buttonOne");
+
+btnOne.addEventListener('click', function(e){
+    let rock = game("rock");
+    console.log(rock);
+
+    if(rock.slice(0,8) === "You Win!"){
+        addToScore();
+    }else if(rock.slice(0,9) === "You Lose!"){
+        addToComputerScore();
+    }
+
+
+    if(wins > 4 || computerScore > 4){
+        console.log(results());
+    }
+});
+
+
+
+const btnTwo = document.querySelector("#buttonTwo");
+
+btnTwo.addEventListener('click', function(e){
+    let paper = game("paper");
+    console.log(paper);
+    if(paper.slice(0,8) === "You Win!"){
+        addToScore();
+    }else if(paper.slice(0,9) === "You Lose!"){
+        addToComputerScore();
+    }
+
+    if(wins > 4 || computerScore > 4){
+        console.log(results());
+    }
+});
+
+
+
+const btnThree = document.querySelector("#buttonThree");
+
+btnThree.addEventListener('click', function(e){
+    let scissors = game("scissors");
+    console.log(scissors);
+    if(scissors.slice(0,8) === "You Win!"){
+        addToScore();
+    }else if(scissors.slice(0,9) === "You Lose!"){
+        addToComputerScore();
+    }
+
+    if(wins > 4 || computerScore > 4){
+        console.log(results());
+    }
+});
+
+
+
+
+
+
+/*function incrementButton(){
+    let element = document.getElementById("playerScore");
+    let value = element.textContent;
+    value++;
+    console.log(value);
+}
+*/
+
+
+
+
+
+
 function getComputerChoice(){
   let result =  Math.floor(Math.random()*3);
   
@@ -26,17 +102,17 @@ function playRound(playerSelection, computerSelection){
 if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
     return "Tie!";
 }else if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper"){
-    return "You Lose!";
+    return "You Lose! Paper beats Rock!";
 }else if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors"){
-    return "You Win!";
+    return "You Win! Rock beats Scissors!";
 }else if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors"){
-    return "You Lose!";
+    return "You Lose! Scissors beats Paper";
 }else if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock"){
-    return "You Win!";
+    return "You Win! Paper beats Rock";
 }else if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock"){
-    return "You Lose!";
+    return "You Lose! Rock beats Scissors";
 }else if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"){
-    return "You Win!";
+    return "You Win! Scissors beats Paper!";
 }
 
 
@@ -49,40 +125,57 @@ if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
 //I am going to set two score variables and add 1 each round depending on who won
 //I will return the results
 
-function game(){
-    let userScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < 5; i++){
-    let choice = prompt("Please enter rock, paper, or scissors!");
-
+function game(choice){
+    
+    
+    console.log("Please enter rock, paper, or scissors!");
+    
     if(choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" ||
      choice.toLowerCase() === "scissors"){
         let computerSelection = getComputerChoice();
         let round = playRound(choice, computerSelection);
        
-    if(round === "You Win!"){
-        userScore ++;
-        console.log("You win this round!")
-    }else if(round === "You Lose!"){
-        computerScore ++;
-        console.log("You lost this round!")
+    if(round.slice(0,8) === "You Win!"){
+        
+        return round;
+        
+        console.log(round)
+    }else if(round.slice(0, 9) === "You Lose!"){
+        
+        return round;
     }else{
-        console.log("Tie!");
+        return "Tie!";
+        
     }
      }else{
         return "Please enter rock, paper, or scissors!"
      }
 
-    }
-    if(userScore > computerScore)
-    {
-        return "You win with a score of " + userScore + " while the computer had " + computerScore;
-    }else if(computerScore > userScore){
-        return "You lost with a score of " + userScore + " while the computer had " + computerScore;
-    }else{
-        return "Tie!";
-    }
+    
+        
+
 
    
 }
-console.log(game());
+
+function results(){
+    if(wins > computerScore)
+    {
+        return "You win with a score of " + wins + " while the computer had " + computerScore;
+    }else if(computerScore > userScore){
+        return "You lost with a score of " + wins + " while the computer had " + computerScore;
+    }else{
+        return "Tie!";
+    }
+}
+
+function addToScore(){
+    wins++;
+    document.getElementById("score").textContent = wins;
+}
+
+function addToComputerScore(){
+    computerScore++;
+    document.getElementById("computerScore").textContent = computerScore;
+}
+//Reviting Rock button and functions work!!!!!
